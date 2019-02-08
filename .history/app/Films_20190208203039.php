727c0103591f020b8e63392356fcb6b0a5958d77
smk87 <30999133+smk87@users.noreply.github.com>
2019-02-08 20:31:11 +0600

    database model added.

---
+++ .history/app/Films_20190208203039.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Films extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description', 'release_date', 'rating', 'ticket_price', 'country', 'name', 'photo'
+    ];
+
+    protected $casts = [
+        'genre' => 'array'
+    ];
+}
---
+++ .history/database/migrations/2019_02_08_142820_films_20190208202934.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Films extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->date('release_date');
+            $table->integer('rating');
+            $table->string('ticket_price');
+            $table->string('country');
+            $table->string('name');
+            $table->json('genre');
+            $table->text('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('films');
+    }
+}
